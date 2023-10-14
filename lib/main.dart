40f296e527caecce209ab54703f8f1104dc5b03b
Colk-tech <master@colk.dev>
2023-10-14 13:01:59 +0900

    add: Add MBTI Compatibility Check App

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,394 @@
+// ignore_for_file: constant_identifier_names, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+enum MBTIType {
+  ENTP,
+  INTP,
+  ENTJ,
+  INTJ,
+  ENFP,
+  INFP,
+  ENFJ,
+  INFJ,
+  ESTJ,
+  ISTJ,
+  ESFJ,
+  ISFJ,
+  ESTP,
+  ESFP,
+  ISTP,
+  ISFP
+}
+
+final compatibilityScores = {
+  MBTIType.INTP: {
+    MBTIType.ENTP: 2,
+    MBTIType.INTP: 4,
+    MBTIType.ENTJ: 4,
+    MBTIType.INTJ: 3,
+    MBTIType.ENFP: 3,
+    MBTIType.INFP: 3,
+    MBTIType.ENFJ: 2,
+    MBTIType.INFJ: 2,
+    MBTIType.ESTJ: 2,
+    MBTIType.ISTJ: 2,
+    MBTIType.ESFJ: 1,
+    MBTIType.ISFJ: 4,
+    MBTIType.ESTP: 4,
+    MBTIType.ESFP: 5,
+    MBTIType.ISTP: 4,
+    MBTIType.ISFP: 3
+  },
+  MBTIType.ENTJ: {
+    MBTIType.ENTP: 3,
+    MBTIType.INTP: 4,
+    MBTIType.ENTJ: 4,
+    MBTIType.INTJ: 2,
+    MBTIType.ENFP: 2,
+    MBTIType.INFP: 2,
+    MBTIType.ENFJ: 4,
+    MBTIType.INFJ: 4,
+    MBTIType.ESTJ: 3,
+    MBTIType.ISTJ: 3,
+    MBTIType.ESFJ: 3,
+    MBTIType.ISFJ: 5,
+    MBTIType.ESTP: 2,
+    MBTIType.ESFP: 4,
+    MBTIType.ISTP: 2,
+    MBTIType.ISFP: 1
+  },
+  MBTIType.INTJ: {
+    MBTIType.ENTP: 4,
+    MBTIType.INTP: 3,
+    MBTIType.ENTJ: 2,
+    MBTIType.INTJ: 4,
+    MBTIType.ENFP: 2,
+    MBTIType.INFP: 2,
+    MBTIType.ENFJ: 4,
+    MBTIType.INFJ: 4,
+    MBTIType.ESTJ: 3,
+    MBTIType.ISTJ: 3,
+    MBTIType.ESFJ: 5,
+    MBTIType.ISFJ: 3,
+    MBTIType.ESTP: 2,
+    MBTIType.ESFP: 1,
+    MBTIType.ISTP: 2,
+    MBTIType.ISFP: 4
+  },
+  MBTIType.ENFP: {
+    MBTIType.ENTP: 3,
+    MBTIType.INTP: 3,
+    MBTIType.ENTJ: 2,
+    MBTIType.INTJ: 2,
+    MBTIType.ENFP: 4,
+    MBTIType.INFP: 2,
+    MBTIType.ENFJ: 3,
+    MBTIType.INFJ: 4,
+    MBTIType.ESTJ: 4,
+    MBTIType.ISTJ: 1,
+    MBTIType.ESFJ: 2,
+    MBTIType.ISFJ: 2,
+    MBTIType.ESTP: 3,
+    MBTIType.ESFP: 4,
+    MBTIType.ISTP: 5,
+    MBTIType.ISFP: 4
+  },
+  MBTIType.INFP: {
+    MBTIType.ENTP: 3,
+    MBTIType.INTP: 3,
+    MBTIType.ENTJ: 2,
+    MBTIType.INTJ: 2,
+    MBTIType.ENFP: 2,
+    MBTIType.INFP: 4,
+    MBTIType.ENFJ: 4,
+    MBTIType.INFJ: 3,
+    MBTIType.ESTJ: 1,
+    MBTIType.ISTJ: 4,
+    MBTIType.ESFJ: 2,
+    MBTIType.ISFJ: 2,
+    MBTIType.ESTP: 5,
+    MBTIType.ESFP: 4,
+    MBTIType.ISTP: 3,
+    MBTIType.ISFP: 4
+  },
+  MBTIType.ENFJ: {
+    MBTIType.ENTP: 2,
+    MBTIType.INTP: 2,
+    MBTIType.ENTJ: 4,
+    MBTIType.INTJ: 4,
+    MBTIType.ENFP: 3,
+    MBTIType.INFP: 4,
+    MBTIType.ENFJ: 4,
+    MBTIType.INFJ: 2,
+    MBTIType.ESTJ: 3,
+    MBTIType.ISTJ: 5,
+    MBTIType.ESFJ: 3,
+    MBTIType.ISFJ: 3,
+    MBTIType.ESTP: 4,
+    MBTIType.ESFP: 2,
+    MBTIType.ISTP: 1,
+    MBTIType.ISFP: 2
+  },
+  MBTIType.INFJ: {
+    MBTIType.ENTP: 2,
+    MBTIType.INTP: 2,
+    MBTIType.ENTJ: 4,
+    MBTIType.INTJ: 4,
+    MBTIType.ENFP: 4,
+    MBTIType.INFP: 3,
+    MBTIType.ENFJ: 2,
+    MBTIType.INFJ: 4,
+    MBTIType.ESTJ: 5,
+    MBTIType.ISTJ: 3,
+    MBTIType.ESFJ: 3,
+    MBTIType.ISFJ: 3,
+    MBTIType.ESTP: 1,
+    MBTIType.ESFP: 2,
+    MBTIType.ISTP: 4,
+    MBTIType.ISFP: 2
+  },
+  MBTIType.ESTJ: {
+    MBTIType.ENTP: 2,
+    MBTIType.INTP: 2,
+    MBTIType.ENTJ: 3,
+    MBTIType.INTJ: 3,
+    MBTIType.ENFP: 4,
+    MBTIType.INFP: 1,
+    MBTIType.ENFJ: 3,
+    MBTIType.INFJ: 5,
+    MBTIType.ESTJ: 4,
+    MBTIType.ISTJ: 2,
+    MBTIType.ESFJ: 4,
+    MBTIType.ISFJ: 4,
+    MBTIType.ESTP: 3,
+    MBTIType.ESFP: 2,
+    MBTIType.ISTP: 4,
+    MBTIType.ISFP: 2
+  },
+  MBTIType.ISTJ: {
+    MBTIType.ENTP: 2,
+    MBTIType.INTP: 2,
+    MBTIType.ENTJ: 3,
+    MBTIType.INTJ: 3,
+    MBTIType.ENFP: 1,
+    MBTIType.INFP: 4,
+    MBTIType.ENFJ: 5,
+    MBTIType.INFJ: 3,
+    MBTIType.ESTJ: 2,
+    MBTIType.ISTJ: 4,
+    MBTIType.ESFJ: 4,
+    MBTIType.ISFJ: 4,
+    MBTIType.ESTP: 4,
+    MBTIType.ESFP: 2,
+    MBTIType.ISTP: 3,
+    MBTIType.ISFP: 2
+  },
+  MBTIType.ESFJ: {
+    MBTIType.ENTP: 4,
+    MBTIType.INTP: 1,
+    MBTIType.ENTJ: 3,
+    MBTIType.INTJ: 5,
+    MBTIType.ENFP: 2,
+    MBTIType.INFP: 2,
+    MBTIType.ENFJ: 3,
+    MBTIType.INFJ: 3,
+    MBTIType.ESTJ: 4,
+    MBTIType.ISTJ: 4,
+    MBTIType.ESFJ: 4,
+    MBTIType.ISFJ: 2,
+    MBTIType.ESTP: 2,
+    MBTIType.ESFP: 3,
+    MBTIType.ISTP: 2,
+    MBTIType.ISFP: 4
+  },
+  MBTIType.ISFJ: {
+    MBTIType.ENTP: 1,
+    MBTIType.INTP: 4,
+    MBTIType.ENTJ: 5,
+    MBTIType.INTJ: 3,
+    MBTIType.ENFP: 2,
+    MBTIType.INFP: 2,
+    MBTIType.ENFJ: 3,
+    MBTIType.INFJ: 3,
+    MBTIType.ESTJ: 4,
+    MBTIType.ISTJ: 4,
+    MBTIType.ESFJ: 2,
+    MBTIType.ISFJ: 4,
+    MBTIType.ESTP: 2,
+    MBTIType.ESFP: 4,
+    MBTIType.ISTP: 2,
+    MBTIType.ISFP: 3
+  },
+  MBTIType.ESTP: {
+    MBTIType.ENTP: 4,
+    MBTIType.INTP: 4,
+    MBTIType.ENTJ: 2,
+    MBTIType.INTJ: 2,
+    MBTIType.ENFP: 3,
+    MBTIType.INFP: 5,
+    MBTIType.ENFJ: 4,
+    MBTIType.INFJ: 1,
+    MBTIType.ESTJ: 3,
+    MBTIType.ISTJ: 4,
+    MBTIType.ESFJ: 2,
+    MBTIType.ISFJ: 2,
+    MBTIType.ESTP: 4,
+    MBTIType.ESFP: 3,
+    MBTIType.ISTP: 2,
+    MBTIType.ISFP: 3
+  },
+  MBTIType.ESFP: {
+    MBTIType.ENTP: 3,
+    MBTIType.INTP: 5,
+    MBTIType.ENTJ: 4,
+    MBTIType.INTJ: 1,
+    MBTIType.ENFP: 4,
+    MBTIType.INFP: 4,
+    MBTIType.ENFJ: 2,
+    MBTIType.INFJ: 2,
+    MBTIType.ESTJ: 2,
+    MBTIType.ISTJ: 2,
+    MBTIType.ESFJ: 3,
+    MBTIType.ISFJ: 4,
+    MBTIType.ESTP: 3,
+    MBTIType.ESFP: 4,
+    MBTIType.ISTP: 3,
+    MBTIType.ISFP: 2
+  },
+  MBTIType.ISTP: {
+    MBTIType.ENTP: 4,
+    MBTIType.INTP: 4,
+    MBTIType.ENTJ: 2,
+    MBTIType.INTJ: 2,
+    MBTIType.ENFP: 5,
+    MBTIType.INFP: 3,
+    MBTIType.ENFJ: 1,
+    MBTIType.INFJ: 4,
+    MBTIType.ESTJ: 4,
+    MBTIType.ISTJ: 3,
+    MBTIType.ESFJ: 2,
+    MBTIType.ISFJ: 2,
+    MBTIType.ESTP: 2,
+    MBTIType.ESFP: 3,
+    MBTIType.ISTP: 4,
+    MBTIType.ISFP: 3
+  },
+  MBTIType.ISFP: {
+    MBTIType.ENTP: 5,
+    MBTIType.INTP: 3,
+    MBTIType.ENTJ: 1,
+    MBTIType.INTJ: 4,
+    MBTIType.ENFP: 4,
+    MBTIType.INFP: 4,
+    MBTIType.ENFJ: 2,
+    MBTIType.INFJ: 2,
+    MBTIType.ESTJ: 2,
+    MBTIType.ISTJ: 2,
+    MBTIType.ESFJ: 4,
+    MBTIType.ISFJ: 3,
+    MBTIType.ESTP: 3,
+    MBTIType.ESFP: 2,
+    MBTIType.ISTP: 3,
+    MBTIType.ISFP: 4
+  },
+};
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'MBTI Compatibility Checker',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  MBTIType? myMBTIType;
+  MBTIType? partnerMBTIType;
+  int? displayedScore;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int getCompatibilityScore() {
+    if (myMBTIType == null || partnerMBTIType == null) {
+      return 0;
+    } else {
+      return compatibilityScores[myMBTIType]![partnerMBTIType]!;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('MBTI Compatibility Checker'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text("My MBTI type is:"),
+            DropdownButton<MBTIType>(
+              value: myMBTIType,
+              onChanged: (newValue) {
+                setState(() {
+                  myMBTIType = newValue!;
+                  displayedScore = null; // タイプが変更されるとスコアをリセット
+                });
+              },
+              items: MBTIType.values
+                  .map((e) => DropdownMenuItem(
+                      value: e, child: Text(e.toString().split('.').last)))
+                  .toList(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 20),
+            const Text("Partner's MBTI type is:"),
+            DropdownButton<MBTIType>(
+              value: partnerMBTIType,
+              onChanged: (newValue) {
+                setState(() {
+                  partnerMBTIType = newValue!;
+                  displayedScore = null; // タイプが変更されるとスコアをリセット
+                });
+              },
+              items: MBTIType.values
+                  .map((e) => DropdownMenuItem(
+                      value: e, child: Text(e.toString().split('.').last)))
+                  .toList(),
             ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  displayedScore = getCompatibilityScore(); // スコアを更新
+                });
+              },
+              child: const Text('Check Compatibility'),
+            ),
+            const SizedBox(height: 20),
+            if (displayedScore != null) // スコアがnullでない場合のみ表示
+              Text(
+                'Compatibility: ${"★" * displayedScore!}${"☆" * (5 - displayedScore!)}',
+                style: const TextStyle(fontSize: 24),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
